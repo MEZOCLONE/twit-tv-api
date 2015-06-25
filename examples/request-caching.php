ce4575e36a94c3ebba719efd91a6e5b7f53a3e4e
Jeff Brand <jeff@deltafactory.com>
2015-06-25 10:51:09 -0400

    Sync caching example with simple-request.php

--- examples/request-caching.php
+++ examples/request-caching.php
@@ -1,5 +1,18 @@
 <?php
+/*
+	TWiT.tv API Example: A Simple Request with Caching
+	by Jeff Brand
 
+	This script demonstrates basic caching of API requests starting with the call demonstrated in simple-request.php.
+
+	More Info:
+	* API credentials: https://twit.tv/about/developer-program
+	* API documentation: http://docs.twittv.apiary.io/
+
+	This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+*/
+
+// Basics
 $app_id = 'YOUR APP ID HERE';
 $app_key = 'YOUR APP KEY HERE';
 $url = 'https://twit.tv/api/v1.0/episodes?range=1';
@@ -8,7 +21,7 @@ $url = 'https://twit.tv/api/v1.0/episodes?range=1';
 $cache_path = __DIR__ . '/cache';
 
 // Only make the HTTP request if we can't find a recent copy locally.
-if ( ! $response = get_from_cache( $url ) ) {
+if ( ! $response = get_from_cache( $cache_path, $url ) ) {
 
 	// Build HTTP headers for curl with API credentials
 	$headers = array(
@@ -27,21 +40,29 @@ if ( ! $response = get_from_cache( $url ) ) {
 		CURLOPT_RETURNTRANSFER => true
 	) );
 
-	//   Make actual HTTPS request.
-	$response = curl_exec( $ch );
+	//   Make the request and store the JSON response.
+	$json = curl_exec( $ch );
 
-	//   Clean up.
+	//   Close cURL handle resource.
 	curl_close( $ch );
 }
 
 // Turn JSON response into a PHP object
-$json = json_decode( $response );
+$response_obj = json_decode( $json );
 
-// Grab the parts we need, do something useful with it!
-$episode = $json->episodes[0];
-$show = $episode->shows[0];
+// Check that the response was JSON and was processed correctly.
+if ( $response_obj !== null ) {
 
-echo 'The most recent episode is ' htmlspecialchars( $show->label . ': ' . $episode->label );
+	// Grab the parts we need, do something useful with it!
+	$episode = $response_obj->episodes[0];
+	$show = $episode->_embedded->shows[0];
+
+	echo 'The most recent episode is ' . htmlspecialchars( $show->label . ': ' . $episode->label ) . "\n";
+} else {
+
+	// Something went wrong. Display an error message.
+	echo "No response. Check your App ID, Key, and request URL for errors.\n";
+}
 
 // Basic caching 
 function get_from_cache( $path, $url ) {
